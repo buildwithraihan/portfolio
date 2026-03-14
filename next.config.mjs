3f9587cd97a8e97853dc5b1390142948b01d734b
Raihan Harismanzil <raihanharismanzil@gmail.com>
2026-03-14 15:33:11 +0530

    chore: revert to Vercel-compatible config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true
-  },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-  reactStrictMode: true
+  reactStrictMode: true,
 };
 
 export default nextConfig;
